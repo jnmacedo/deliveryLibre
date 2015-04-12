cc52e1cf15ebcade63dd96845d595a21d60a0b31
Diego Maidana <diegomaidana2@gmail.com>
2015-04-12 09:28:25 -0300

    borro
    borro

--- DLibre/celda.swift
+++
@@ -1,8 +0,0 @@
-//
-//  celda.swift
-//  DLibre
-//
-//  Created by Damian Pintos on 12/4/15.
-//  Copyright (c) 2015 Damian Pintos. All rights reserved.
-//
-
--- DLibre/test.swift
+++
@@ -1,9 +0,0 @@
-//
-//  test.swift
-//  DLibre
-//
-//  Created by Damian Pintos on 12/4/15.
-//  Copyright (c) 2015 Damian Pintos. All rights reserved.
-//
-
-import Foundation
